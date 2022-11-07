c6d54c7ceefd801927726e0219021731fbbc23e1
nicole sue lynn leaw <nicoleleaaw@gmail.com>
2022-11-07 16:07:23 +1100

    trying to fix 3

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ for (let link of links) {
     link.addEventListener('click', selectLink);
     link.addEventListener('click', scrollToSection);
 }
+
+function show() {
+    var x = document.getElementById("buttonpdf");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function show2() {
+    var x = document.getElementById("buttonpdf2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
